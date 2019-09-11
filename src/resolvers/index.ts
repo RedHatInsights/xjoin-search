d1be0b0aee65787b3a7222b50741099dc573d265
Jozef Hartinger <jharting@redhat.com>
2019-09-11 09:56:24 +0200

    add JSONObject for dynamic maps

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,9 +1,13 @@
+import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json';
 import hosts from './hosts';
 
 const resolvers = {
     Query: {
         hosts
-    }
+    },
+
+    JSON: GraphQLJSON,
+    JSONObject: GraphQLJSONObject
 };
 
 export default resolvers;
