3beddf9c176faaecfed409acd63baa0f5c126c64
Josef Hak <jhak@redhat.com>
2019-10-10 16:11:38 +0200

    added Verify stage to Jenkinsfile

---
+++ build/utils.groovy
@@ -0,0 +1,40 @@
+def scale (NS, count) {
+    sh "oc scale --replicas=${count} --namespace=${NS} dc/elasticsearch"
+
+    // wait for dependencies
+    waitFor(NS, count, 'elasticsearch')
+}
+
+def waitFor (NS, count, resource) {
+    openshift.withCluster() {
+        openshift.withProject(NS) {
+            timeout(2) {
+                def latestDeploymentVersion = openshift.selector('dc', resource).object().status.latestVersion
+                def ready = { openshift.selector('rc', "${resource}-${latestDeploymentVersion}").object().status.readyReplicas ?: 0 }
+
+                while (ready() != count) {
+                    println("Waiting for ${resource} to scale to ${count} (${ready()} available)")
+                    sleep (5)
+                }
+
+                println("${resource} succesfully scaled to ${ready()} replicas")
+            }
+        }
+    }
+}
+
+def withScaledEnv(NS, Closure step) {
+    lock(NS) {
+        try {
+            scale(NS, 1)
+            sh "oc get pods --namespace ${NS}"
+
+            step()
+        } finally {
+            scale(NS, 0)
+            sh "oc get pods --namespace ${NS}"
+        }
+    }
+}
+
+return this;
