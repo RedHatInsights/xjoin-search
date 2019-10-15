4e5d0c348a56a95c783dc22e40eafe11a59b3f0b
Josef Hak <jhak@redhat.com>
2019-10-15 14:12:50 +0200

    used identity middleware in the integ. tests

--- src/resolvers/hosts/hosts.integration.ts
+++ src/resolvers/hosts/hosts.integration.ts
@@ -1,4 +1,4 @@
-import { getQuery } from '../../../test';
+import { runQuery } from '../../../test';
 
 const BASIC_QUERY = `
     query hosts ($filter: HostFilter, $order_by: HOSTS_ORDER_BY, $order_how: ORDER_DIR) {
@@ -15,19 +15,15 @@ const BASIC_QUERY = `
     }
 `;
 
-function runBasicQuery (variables: Record<string, any>) {
-    return getQuery()({ query: BASIC_QUERY, variables });
-}
-
 describe('hosts query', function () {
     test('fetch hosts', async () => {
-        const data = await runBasicQuery({});
-
+        const { data, status } = await runQuery(BASIC_QUERY, {});
+        expect(status).toEqual(200);
         expect(data).toMatchSnapshot();
     });
 
     test('fetch host with canonical facts', async () => {
-        const data = await getQuery()({ query: `
+        const { data, status } = await runQuery(`
             {
                 hosts (
                     filter: {
@@ -38,80 +34,83 @@ describe('hosts query', function () {
                         id, canonical_facts
                     }
                 }
-            }`
-        });
-
+            }`,
+        {});
+        expect(status).toEqual(200);
         expect(data).toMatchSnapshot();
     });
 
     describe ('ordering', function () {
 
         test('display_name ASC', async () => {
-            const result = await runBasicQuery({
+            const { data, status } = await runQuery(BASIC_QUERY, {
                 order_by: 'display_name',
                 order_how: 'ASC'
             });
 
-            expect(result).toMatchSnapshot();
+            expect(status).toEqual(200);
+            expect(data).toMatchSnapshot();
         });
 
         test('display_name DESC', async () => {
-            const result = await runBasicQuery({
+            const { data, status } = await runQuery(BASIC_QUERY, {
                 order_by: 'display_name',
                 order_how: 'DESC'
             });
 
-            expect(result).toMatchSnapshot();
+            expect(status).toEqual(200);
+            expect(data).toMatchSnapshot();
         });
 
         test('modified_on DESC', async () => {
-            const result = await runBasicQuery({
+            const { data, status } = await runQuery(BASIC_QUERY, {
                 order_by: 'modified_on',
                 order_how: 'DESC'
             });
 
-            expect(result).toMatchSnapshot();
+            expect(status).toEqual(200);
+            expect(data).toMatchSnapshot();
         });
     });
 
     describe('queries', function () {
         describe('display_name', function () {
             test('substring', async () => {
-                const result = await runBasicQuery({ filter: { display_name: '*est03.*' }});
-                result.data.hosts.data.should.have.length(1);
-                result.data.hosts.data[0].id.should.equal('f5ac67e1-ad65-4b62-bc27-845cc6d4bcee');
+                const { data } = await runQuery(BASIC_QUERY, { filter: { display_name: '*est03.*' }});
+                data.hosts.data.should.have.length(1);
+                data.hosts.data[0].id.should.equal('f5ac67e1-ad65-4b62-bc27-845cc6d4bcee');
             });
         });
 
         describe('fqdn', function () {
             test('substring', async () => {
-                const result = await runBasicQuery({ filter: { fqdn: '*dn.test02.*' }});
-                result.data.hosts.data.should.have.length(1);
-                result.data.hosts.data[0].id.should.equal('22cd8e39-13bb-4d02-8316-84b850dc5136');
+                const { data } = await runQuery(BASIC_QUERY, { filter: { fqdn: '*dn.test02.*' }});
+                data.hosts.data.should.have.length(1);
+                data.hosts.data[0].id.should.equal('22cd8e39-13bb-4d02-8316-84b850dc5136');
             });
         });
 
         describe('insights_id', function () {
             test('exact match', async () => {
-                const result = await runBasicQuery({ filter: { insights_id: '17c52679-f0b9-4e9b-9bac-a3c7fae5070c' }});
-                result.data.hosts.data.should.have.length(1);
-                result.data.hosts.data[0].id.should.equal('22cd8e39-13bb-4d02-8316-84b850dc5136');
+                const { data } = await runQuery(BASIC_QUERY, { filter: { insights_id: '17c52679-f0b9-4e9b-9bac-a3c7fae5070c' }});
+                data.hosts.data.should.have.length(1);
+                data.hosts.data[0].id.should.equal('22cd8e39-13bb-4d02-8316-84b850dc5136');
             });
 
             test('substring no wildcards', async () => {
-                const result = await runBasicQuery({ filter: { insights_id: 'a3c7fae507' }});
-                result.data.hosts.data.should.have.length(0);
+                const { data } = await runQuery(BASIC_QUERY, { filter: { insights_id: 'a3c7fae507' }});
+                data.hosts.data.should.have.length(0);
             });
 
             test('substring', async () => {
-                const result = await runBasicQuery({ filter: { insights_id: '*a3c7fae507*' }});
-                result.data.hosts.data.should.have.length(1);
-                result.data.hosts.data[0].id.should.equal('22cd8e39-13bb-4d02-8316-84b850dc5136');
+                const { data } = await runQuery(BASIC_QUERY, { filter: { insights_id: '*a3c7fae507*' }});
+                data.hosts.data.should.have.length(1);
+                data.hosts.data[0].id.should.equal('22cd8e39-13bb-4d02-8316-84b850dc5136');
             });
         });
 
         test('compound', async () => {
-            const result = await runBasicQuery({
+            const { data } = await runQuery(BASIC_QUERY, {
                 filter: {
                     OR: [{
                         fqdn: '*dn.test02.rhel7.jharting.local',
@@ -122,17 +121,17 @@ describe('hosts query', function () {
                     }]
                 }
             });
-            expect(result).toMatchSnapshot();
+            expect(data).toMatchSnapshot();
         });
 
         describe('system_profile', function () {
             test('arch', async () => {
-                const result = await runBasicQuery({ filter: { spf_arch: 'x86_64' }});
-                expect(result).toMatchSnapshot();
+                const { data } = await runQuery(BASIC_QUERY, { filter: { spf_arch: 'x86_64' }});
+                expect(data).toMatchSnapshot();
             });
 
             test('os_release', async () => {
-                const result = await runBasicQuery({
+                const { data } = await runQuery(BASIC_QUERY, {
                     filter: {
                         spf_os_release: '7.*',
                         NOT: {
@@ -140,24 +139,24 @@ describe('hosts query', function () {
                         }
                     }
                 });
-                expect(result).toMatchSnapshot();
+                expect(data).toMatchSnapshot();
             });
 
             test('os_kernel_release', async () => {
-                const result = await runBasicQuery({ filter: { spf_os_kernel_version: '4.18.*' }});
-                result.data.hosts.data.should.have.length(1);
-                result.data.hosts.data[0].id.should.equal('22cd8e39-13bb-4d02-8316-84b850dc5136');
+                const { data } = await runQuery(BASIC_QUERY, { filter: { spf_os_kernel_version: '4.18.*' }});
+                data.hosts.data.should.have.length(1);
+                data.hosts.data[0].id.should.equal('22cd8e39-13bb-4d02-8316-84b850dc5136');
             });
 
             test('os_infrastructure_type', async () => {
-                const result = await runBasicQuery({ filter: { spf_infrastructure_type: 'virtual' }});
-                expect(result).toMatchSnapshot();
+                const { data } = await runQuery(BASIC_QUERY, { filter: { spf_infrastructure_type: 'virtual' }});
+                expect(data).toMatchSnapshot();
             });
 
             test('os_infrastructure_vendor', async () => {
-                const result = await runBasicQuery({ filter: { spf_infrastructure_vendor: 'baremetal' }});
-                result.data.hosts.data.should.have.length(1);
-                result.data.hosts.data[0].id.should.equal('f5ac67e1-ad65-4b62-bc27-845cc6d4bcee');
+                const { data } = await runQuery(BASIC_QUERY, { filter: { spf_infrastructure_vendor: 'baremetal' }});
+                data.hosts.data.should.have.length(1);
+                data.hosts.data[0].id.should.equal('f5ac67e1-ad65-4b62-bc27-845cc6d4bcee');
             });
         });
     });
--- test/index.ts
+++ test/index.ts
@@ -1,14 +1,14 @@
 import 'should';
 import start from '../src/app';
-import { createTestClient } from 'apollo-server-testing';
+import createIdentityHeader from '../src/middleware/identity/utils';
+import * as constants from '../src/constants';
+import { GraphQLClient } from 'graphql-request';
 
-let ctx: any = null;
+let app: any = null;
 
 beforeAll(async () => {
     try {
-        const app = await start();
-        const { query } = createTestClient(app.apollo);
-        ctx = { app, query };
+        app = await start();
     } catch (e) {
         // workaround for https://github.com/facebook/jest/issues/8688
         console.log(e);
@@ -17,11 +17,11 @@ beforeAll(async () => {
 });
 
 afterAll(async () => {
-    const local = ctx;
-    ctx = null;
-    await local.app.stop();
+    await app.stop();
 });
 
-export function getQuery () {
-    return ctx.query;
+export async function runQuery(query: string, variables: Record<string, any>, modify_identity_hdr_data = (d: any) => d) {
+    const headers = { headers: { [constants.IDENTITY_HEADER]: createIdentityHeader(modify_identity_hdr_data)}};
+    const client = new GraphQLClient('http://localhost:4000/graphql', headers);
+    return client.rawRequest(query, variables);
 }
