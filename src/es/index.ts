ff9fa385177b6f59ff04e7249a0a0510c6b9b30e
Josef Hak <jhak@redhat.com>
2019-10-23 14:13:51 +0200

    Elastic login data loaded separately

--- src/es/index.ts
+++ src/es/index.ts
@@ -1,5 +1,6 @@
 import { Client } from '@elastic/elasticsearch';
 import config from '../config';
 
-const client = new Client({ node: config.es.nodes });
+const client = new Client({ node: config.es.nodes, auth: {username: config.es.username,
+    password: config.es.password}});
 export default client;
