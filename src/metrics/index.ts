b8c4774aabccfc5372bd24af03b4b7d828dea04d
Josef Hak <jhak@redhat.com>
2019-11-11 10:42:08 +0100

    exposed metrics using 'express-prom-bundle'

--- src/metrics/index.ts
+++ src/metrics/index.ts
@@ -1,18 +1,33 @@
 import * as client from 'prom-client';
 import config from '../config';
 import { Request, Response, Application } from 'express';
+import promBundle from 'express-prom-bundle';
 
 export { client };
 
 export default function start (app: Application) {
-    client.collectDefaultMetrics({
-        prefix: config.metrics.prefix,
-        timeout: 5000
-    });
-
     app.get('/metrics', (req: Request, res: Response) => {
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.write(client.register.metrics());
         res.end();
     });
+
+    const prefix = config.metrics.prefix;
+    const opts = {
+        httpDurationMetricName: `${prefix}http_request_duration_seconds`,
+        includeUp: false,
+        includeMethod: false,
+        includePath: false,
+        includeStatusCode: true,
+
+        promClient: {
+            collectDefaultMetrics: {
+                prefix
+            }
+        }
+    };
+
+    const metricsMiddleware = promBundle(opts);
+
+    app.use(metricsMiddleware);
 }
