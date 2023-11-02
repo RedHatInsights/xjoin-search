44e7ece3a6946b17f64b2faa27060b85233667c3
Stephen Adams <stephen.adams@redhat.com>
2023-11-02 11:08:02 -0400

    fix Sec Comp tag
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- build_deploy.sh
+++ build_deploy.sh
@@ -5,7 +5,7 @@ set -exv
 IMAGE="quay.io/cloudservices/xjoin-search"
 IMAGE_TAG=$(git rev-parse --short=7 HEAD)
 BUILD_COMMIT=$(git rev-parse HEAD)
-SECURITY_COMPLIANCE_TAG="security-compliance"
+SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)-$(git rev-parse --short=7 HEAD)"
 
 if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
     echo "QUAY_USER and QUAY_TOKEN must be set"
